dde3abbbbafc2fd920379b2844bcc6a8f4393ec5
Romero Albarracin <s4945948@bns.bns>
2018-07-25 15:14:10 -0500

    fail test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -21,7 +21,7 @@ public class TestGreeter {
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
 
-    assertThat(greeter.greet(someone), containsString(""));
+    assertThat(greeter.greet(someone), containsString("jojo"));
   }
 
   @Test
